5c973f6b71acc0b838a762c9fad0913a86d9b07b
João Correia <155659371+Joao-VCorreia@users.noreply.github.com>
2024-06-10 21:28:52 -0300

    Aprimora e adiciona informações na base de dados fictícia

--- database/prontuarios-dataset.py
+++ database/prontuarios-dataset.py
@@ -3,11 +3,15 @@ import numpy as np
 import random as rd
 from datetime import time
 
-dfPacientes = pd.read_csv(r'database\medical-records.csv')
+dfPacientes = pd.read_csv("medical-records.csv")
 
-dfPacientes = dfPacientes.iloc[:400]
+dfPacientes = dfPacientes.iloc[:500]
+
 dfPacientes.drop(['patient_id', 'medical_conditions', 'medications', 'allergies'], axis=1, inplace=True)
 dfPacientes.rename(columns={'name':'nome', 'date_of_birth':'dataNascimento', 'gender':'sexo', 'last_appointment_date': 'ultimaConsulta'}, inplace=True)
+
+dfPacientes['sexo'].replace({'F': 'Feminino', 'M': 'Masculino'}, inplace=True)
+
 dfPacientes = dfPacientes[['nome', 'sexo', 'dataNascimento', 'ultimaConsulta']]
 
 letrasAZ = [chr(i) for i in range(65, 91)]
@@ -25,9 +29,9 @@ for letra in letrasAZ:
     listaClassificacao.append(classe)
 
 
-dfMedicamentos = pd.DataFrame({'medicacao':listaMedicamentos, 'classe': listaClassificacao})
+dfMedicamentos = pd.DataFrame({'medicacao':listaMedicamentos, 'classe': listaClassificacao}).reset_index(drop=True)
 
-lista = np.random.randint(1,5, size=len(dfPacientes))
+lista = np.random.randint(1,10, size=len(dfPacientes))
 dfExpandidoPaciente = dfPacientes.loc[dfPacientes.index.repeat(lista)].reset_index(drop=True)
 
 dfExpandidoPaciente['medicacao'] = np.random.choice(listaMedicamentos, size=len(dfExpandidoPaciente))
@@ -36,4 +40,9 @@ dfPacientesMedicamentos = dfExpandidoPaciente.merge(dfMedicamentos, how = 'left'
 
 dfPacientesMedicamentos['horario'] = [time(hour=h) for h in np.random.choice(range(0,24,2), len(dfPacientesMedicamentos))]
 
-dfPacientesMedicamentos.to_parquet('database/banco-de-dados.parquet')
+nomesPacientes = dfPacientesMedicamentos['nome'].unique()
+numeroAtendimento = {nome: np.random.randint(10000000, 99999999) for nome in nomesPacientes}
+
+dfPacientesMedicamentos['atendimento'] = dfPacientesMedicamentos['nome'].map(numeroAtendimento)
+
+dfPacientesMedicamentos.to_parquet('banco-de-dados.parquet')
